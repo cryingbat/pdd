f5f807b257b4983744ba24bad3e50d3f8f299ec2
cryingbat <haoqi0214@126.com>
2019-09-07 13:59:11 +0800

    添加路由模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home';
 // 指令是不带模板的component
 // 注解是对属性的变法  getter  setter
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { HomeContainerComponent } from './component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeContainerComponent
+  ],
   imports: [
     SharedModule,
     HomeRoutingModule
